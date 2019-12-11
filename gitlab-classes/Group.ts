3eee013490dd1364f535e80f2119d6b9cef2ff30
Yoni Water Man <yonixw@gmail.com>
2019-12-11 15:43:53 +0200

    tidy

--- gitlab-classes/Group.ts
+++ gitlab-classes/Group.ts
@@ -2,22 +2,6 @@ import { pathToFileURL } from "url";
 import { Project } from "./Project";
 
 export class Group {
-    /*
-    {                                              
-  "id": 4891969,                               
-  "name": "qp-ui",                             
-  "path": "qp-ui",                             
-  "kind": "group",                             
-  "full_path": "qp-ui",                        
-  "parent_id": null,                           
-  "avatar_url": null,                          
-  "web_url": "https://gitlab.com/groups/qp-ui",
-  "members_count_with_descendants": 2,         
-  "billable_members_count": 2,                 
-  "plan": "free"                               
-},                                             
-    */
-
     id: number;
     name: string;
     path: string;
